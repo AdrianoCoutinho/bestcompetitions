d90f9b25d7e7b8efb26fc7acc9c51e34cd64621a
AdrianoCoutinho <adriano.98@outlook.com>
2024-05-04 19:30:42 -0300

    feat: projeto limpo

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { RedisConnection } from "./main/database/redis.connection";
+import { TypeormConnection } from "./main/database/typeorm.connection";
+import { Server } from "./main/server/express.server";
+
+Promise.all([TypeormConnection.init(), RedisConnection.connect()]).then(
+  Server.run
+);
