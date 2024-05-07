a9a9c487b60bf83d29a63b8cc90bb4a75e68f166
AdrianoCoutinho <adriano.98@outlook.com>
2024-05-07 14:43:45 -0300

    fix: env database

--- src/main/config/typeorm.config.ts
+++ src/main/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import { DataSource } from "typeorm";
 import { databaseEnv } from "../../app/envs/database.env";
 
-let entities = "build/app/shared/database/entities/**/*.js";
-let migrations = "build/app/shared/database/migrations/**/*.js";
+let entities = "src/app/shared/database/entities/**/*.ts";
+let migrations = "src/app/shared/database/migrations/**/*.ts";
 
-if (databaseEnv.apiEnv === "dev") {
-  entities = "src/app/shared/database/entities/**/*.ts";
-  migrations = "src/app/shared/database/migrations/**/*.ts";
+if (databaseEnv.apiEnv === "production") {
+  entities = "build/app/shared/database/entities/**/*.js";
+  migrations = "build/app/shared/database/migrations/**/*.js";
 }
 
 export default new DataSource({
