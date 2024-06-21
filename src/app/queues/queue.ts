2e7f93b3144fc5bf6a195801ae1aa9bb654775f6
AdrianoCoutinho <adriano.98@outlook.com>
2024-06-21 15:32:00 -0300

    fix: job type

--- src/app/queues/queue.ts
+++ src/app/queues/queue.ts
@@ -15,11 +15,11 @@ const clipQueue = new Queue("createClip", redisConfig);
 
 clipQueue.process(createClipJob.handle);
 
-clipQueue.on("completed", (job, result) => {
+clipQueue.on("completed", (job: any, result: any) => {
   console.log(`Job ${job.id} completed with result ${result}`);
 });
 
-clipQueue.on("failed", (job, err) => {
+clipQueue.on("failed", (job: any, err: any) => {
   console.error(`Job ${job.id} failed with error ${err}`);
 });
 
